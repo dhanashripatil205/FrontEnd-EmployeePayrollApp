feee505d5167549cc90aede915781c63a6442936
dhanashripatil205 <107808336+dhanashripatil205@users.noreply.github.com>
2023-02-21 13:44:14 +0530

    UC17-Employee-Payroll-Form-with-Header-and-Name!!!

--- js/home.js
+++ js/home.js
@@ -12,22 +12,66 @@ const createInnerHtml = () => {
     <th>Salary</th>
     <th>Start Date</th>
     <th>Actions</th>`;
+  
+  let innerHtml = `${headerHtml}`;
+  let employeePayrollDataList = createEmployeePayrollJSON();
 
-  const innerHtml = `${headerHtml}
-    <tr>
-      <td><img class="profile" src="../assets/profile-images/profile_img-1.png" alt="profile_img-1"></td>
-      <td>Ashika</td>
-      <td>Female</td>
-      <td>
-        <div class="dept-label">HR</div>
-        <div class="dept-label">Finance</div>
-      </td>
-      <td>3000000</td>
-      <td>12 Oct 2021</td>
-      <td>
-        <img src="../assets/icons/delete.svg" alt="delete" id="1" onclick="remove(this)">
-        <img src="../assets/icons/edit.svg" alt="edit" id="1" onclick="update(this)">
-      </td>
-    </tr>`;
+  for(const employeePayrollData of employeePayrollDataList){
+    
+    innerHtml = `${innerHtml}
+      <tr>
+        <td><img class="profile" src="${employeePayrollData._profileImage}" alt="profile_img-1"></td>
+        <td>${employeePayrollData._name}</td>
+        <td>${employeePayrollData._gender}</td>
+        <td>${getDepartmentHtml(employeePayrollData._department)}</td>
+        
+        <td>${employeePayrollData._salary}</td>
+        <td>${employeePayrollData._startDate}</td>
+        <td>
+          <img src="../assets/icons/delete.svg" alt="delete" id="1" onclick="remove(this)">
+          <img src="../assets/icons/edit.svg" alt="edit" id="1" onclick="update(this)">
+        </td>
+      </tr>`;
+    
+  }
   document.querySelector('#table-display').innerHTML = innerHtml;
+}
+
+const createEmployeePayrollJSON = () => {
+  let employeePayrollListLocal = [
+    {
+      _name: 'Killua',
+      _gender: 'Male',
+      _department: [
+        'HR',
+        'Sales',
+        'Finance'
+      ],
+      _salary: 10000000,
+      _startDate: '18 Sept 2021',
+      _note: 'Killua lives in Kukoro Mountain.',
+      _id: new Date().getTime(),
+      _profileImage: '../assets/profile-images/profile_img-4.png'
+    },
+    {
+      _name: 'Jugemu Jugemu GoKono Surikire',
+      _gender: 'Male',
+      _department: [
+        'Finance'
+      ],
+      _salary: 5000000,
+      _startDate: '30 Sept 2021',
+      _note: 'I have a very long name',
+      _id: new Date().getTime(),
+      _profileImage: '../assets/profile-images/profile_img-2.png'
+    }
+  ];
+  return employeePayrollListLocal;
+}
+const getDepartmentHtml = (departmentList) => {
+  let departmentHtml = '';
+  for(const department of departmentList){
+    departmentHtml = `${departmentHtml} <div class='dept-label'>${department}</div>`
+  }
+  return departmentHtml
 }
