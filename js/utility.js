6e6b4103d10f07bb457bf14965c7ff94a213582c
dhanashripatil205 <107808336+dhanashripatil205@users.noreply.github.com>
2023-02-21 15:06:19 +0530

    UC22-Remove-an-Employee-from-the-Payroll-details!!!

--- js/utility.js
+++ js/utility.js
@@ -3,3 +3,19 @@ const stringifyDate = (date) => {
   const newDate = !date ? "undefined" : new Date(Date.parse(date)).toLocaleDateString('en-GB', options);
   return newDate;
 }
+
+const checkName = (name) => {
+  let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+  if (!nameRegex.test(name))
+    throw "Name is Incorrect";
+}
+
+const checkStartDate = (startDate) => {
+  var today = new Date();
+  if (today < startDate)
+    throw 'Start date is in the Future';
+  const minDate = new Date(today.setDate(today.getDate() - 30));
+  today = new Date();
+  if (startDate < minDate)
+    throw 'Start date is Beyond 30 days';
+}
